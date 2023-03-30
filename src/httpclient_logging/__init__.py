6c3ec0150bbb4794d7a907df3d2f5ab95fabbebb
Wouter Vanden Hove <wouter@libranet.eu>
2023-03-30 13:46:39 +0200

    prepare release

--- src/httpclient_logging/__init__.py
+++ src/httpclient_logging/__init__.py
@@ -1,5 +1,3 @@
 """httpclient_logging.__init__."""
-__version__ = "0.0.0"
+__version__ = "1.0"
 __copyright__ = "Copyright 2023 Libranet - MIT License."
-
-from httpclient_logging.patch import configure
