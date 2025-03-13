43b101ec00a096cdbf6b3eec6e09158952cdda71
Wouter Vanden Hove <wouter@libranet.eu>
2025-03-13 17:53:41 +0100

    support python3.8 and up

--- src/httpclient_logging/patch.py
+++ src/httpclient_logging/patch.py
@@ -3,13 +3,14 @@
 import http.client
 import logging
 import os
+import typing as tp
 
 pre_patched_value = print
 
 log = logging.getLogger(__name__)
 
 
-def set_httpclient_debuglevel(debuglevel: int | str | None = None) -> None:
+def set_httpclient_debuglevel(debuglevel: tp.Union[int, str, None] = None) -> None:  # noqa: UP007
     """Set debug-level for http.client.
 
     If http-debuglevel > 0, debug messages in the http.client.HTTPConnection-class will be printed to STDOUT.
